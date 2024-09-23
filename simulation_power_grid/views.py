2c526ff86acadb5704c0a3de27dd127229aeb262
георгий савинцев <igsgo4645@gmail.com>
2024-09-23 23:06:48 +0400

    add method CREATE

--- simulation_power_grid/views.py
+++ simulation_power_grid/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Transformator
+from .forms import TransformatorForm
 
-# Create your views here.
+
+def transformator_add(request):
+    if request.method == "POST":
+        form = TransformatorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('transformator_list')  # редирект на список
+    else:
+        form = TransformatorForm()
+    return render(request, 'transformator_add.html', {'form': form})
